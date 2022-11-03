0a4b1eaae6a2da6facc5f7a35defb1015090962f
lceCoIdMoon <wehuffman15@gmail.com>
2022-11-02 21:02:51 -0400

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:dynamic_color/dynamic_color.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
